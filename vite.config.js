658c64dbb524809b7d5010300abae12b01feea86
Michael Castrillon <157509742+MichaelCastri@users.noreply.github.com>
2025-04-09 10:07:09 +0200

    miercoles am

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',         // <<<<<<<<<<<<<< 🔥 AÑADIR ESTO
   plugins: [
     react(),
-  tailwindcss(),
+    tailwindcss(),
   ],
-  
 })
